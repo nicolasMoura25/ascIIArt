117cd2ba8cff7c701050db609aedda6707056dde
nicolasMoura25 <nicolas.moura25@gmail.com>
2020-10-22 22:39:15 -0300

    aa

--- main.c
+++ main.c
@@ -83,6 +83,7 @@ char changeToASC(int color)
 int main(int argc, char** argv)
 {
 	int reduction;
+	int l;
 
     Img pic;
     if(argc == 1) {
@@ -91,30 +92,16 @@ int main(int argc, char** argv)
     }
     load(argv[1], &pic);
 
-    //printf("Primeiros 10 pixels da imagem:\n");
-     //for(int i=0; i<10; i++) {
-     //    printf("[%d %d %d] ", pic.img[i].r, pic.img[i].g, pic.img[i].b);
-     //}
-     //printf("\n");
 	printf("pixeis:[%d] " ,sizeof(pic.img));
 	//printf("altura:[%d] e comprimento:[%d] " ,pic.width,pic.height);
 
 	
-	
 
-    // Inverte as cores
-    //for(int i=0; i<pic.width*pic.height; i++) {
-    //    pic.img[i].r = 255 - pic.img[i].r;
-    //   pic.img[i].g = 255 - pic.img[i].g;
-    //  pic.img[i].b = 255 - pic.img[i].b;
-    //}
 
 	// tranforma em tons de cinza
 
 	printf("digite um fator de redução\n");
 	scanf("%d",&reduction);
-
-	
 
 
 	for(int i=0; i<pic.width*pic.height; i++) 
@@ -126,99 +113,89 @@ int main(int argc, char** argv)
     	pic.img[i].b = gray;
 	}
 
-	int l;
-	if(reduction == 50){
-		for(int i=0; i<pic.height; i=i+2){
-			for(int j=0; j<pic.width; j=j+2){
-				pic.img[l].r = pic.img[i*(pic.width) + j].r;
-				pic.img[l].b = pic.img[i*(pic.width) + j].b;
-				pic.img[l].g = pic.img[i*(pic.width) + j].g;
-				l++;
+		if(reduction == 50){
+			for(int i=0; i<pic.height; i=i+2){
+				for(int j=0; j<pic.width; j=j+2){
+					pic.img[l].r = pic.img[i*(pic.width) + j].r;
+					pic.img[l].b = pic.img[i*(pic.width) + j].b;
+					pic.img[l].g = pic.img[i*(pic.width) + j].g;
+					l++;
+				}
 			}
-		}
-	}
+			SOIL_save_image("out.bmp", SOIL_SAVE_TYPE_BMP, pic.width/2, pic.height/2,
+        	3, (const unsigned char*) pic.img);
 
+			FILE* arq = fopen("saida.html", "w"); // criar o arquivo: w
+    		if(arq == NULL) // 0, falso
+    		{
+        		printf("Erro abrindo arquivo de saída\n");
+        		exit(1);
+    		}
+
+    		fprintf(arq, "<html>\n");
+    		fprintf(arq,"<head>\n");
+    		fprintf(arq,"</head>\n");
+    		fprintf(arq,"<body style=\"background:black;\" leftmargin=0; topmargin=0 >");
+    		fprintf(arq,"<style> pre { color: white; font-family:Courier; font-size:6px; letter-spacing:1px}</style>");
+			fprintf(arq,"<pre>");
+			for(int i=0; i<pic.height/4; i++) 
+			{
+				for(int j=0; j<pic.width/2; j++) 
+				{
+					// podemos passar qualquer parametro, pois eles são iguais
+					// multiplico o i pela altura para ter a linha e é somado com a largura 
+					fprintf(arq,"%c", changeToASC(pic.img[(i*pic.width) +j].r));
+				}
+				fprintf(arq,"<br>");
+			}
+			fprintf(arq,"</pre>");
 	
-	/*int cont = 0;
-	unsigned char x;
-	unsigned char grey = 0;
-	int l = 0;
-	int c = 0;
-	int matrix[pic.height][pic.width];
-	if(reduction == 50){
-		for(int i=0; i<pic.height; i=i+2){
-			for(int j=0; j<pic.width; j=j+2){
-			//if(j >= pic.width ){
-			//	cont = cont + (pic.width/2);
-			//}
-			grey = (pic.img[i].r + pic.img[i+1].r + pic.img[i+pic.width].r + pic.img[i+pic.width + 1].r)/4;
-			//grey += pic.img[i+1].r;
-			//gray = grey/510;
-			pic.img[j].r = grey;
-			pic.img[j].g = grey;
-			pic.img[j].b = grey;
-			pic.img[j+1].r = grey;
-			pic.img[j+1].g = grey;
-			pic.img[j+1].b = grey;
-			pic.img[j + i*pic.width].r = grey;
-			pic.img[j + i*pic.width].g = grey;
-			pic.img[j + i*pic.width].b = grey;
+    		fprintf(arq,"</body>\n");
+			fprintf(arq, "</html>\n");
 
-			pic.img[(j+1) + i*pic.width].r = grey;
-			pic.img[(j+1) + i*pic.width].g = grey;
-			pic.img[(j+1) + i*pic.width].b = grey;
-			
-			matrix[l][c] = grey;
-			
-			if(c >= pic.width/2){
-				l++;
-				c = 0;
+    		fclose(arq);
+    		free(pic.img);
+		}else if(reduction == 25){
+			for(int i=0; i<pic.height; i=i+2){
+				for(int j=0; j<pic.width; j=j+4){
+					pic.img[l].r = pic.img[i*(pic.width) + j].r;
+					pic.img[l].b = pic.img[i*(pic.width) + j].b;
+					pic.img[l].g = pic.img[i*(pic.width) + j].g;
+					l++;
+				}
 			}
+			SOIL_save_image("out.bmp", SOIL_SAVE_TYPE_BMP, pic.width, pic.height,
+        	3, (const unsigned char*) pic.img);
 
-			c++;
+			FILE* arq = fopen("saida.html", "w"); // criar o arquivo: w
+    		if(arq == NULL) // 0, falso
+    		{
+        		printf("Erro abrindo arquivo de saída\n");
+        		exit(1);
+    		}
+
+    		fprintf(arq, "<html>\n");
+    		fprintf(arq,"<head>\n");
+    		fprintf(arq,"</head>\n");
+    		fprintf(arq,"<body style=\"background:black;\" leftmargin=0; topmargin=0 >");
+    		fprintf(arq,"<style> pre { color: white; font-family:Courier; font-size:6px; letter-spacing:1px}</style>");
+			fprintf(arq,"<pre>");
+			for(int i=0; i<pic.height/8; i++) 
+			{
+				for(int j=0; j<pic.width/4; j++) 
+				{
+					// podemos passar qualquer parametro, pois eles são iguais
+					// multiplico o i pela altura para ter a linha e é somado com a largura 
+					fprintf(arq,"%c", changeToASC(pic.img[(i*pic.width) +j].r));
+				}
+				fprintf(arq,"<br>");
 			}
-		}
-	}
-	*/
-
-
-    // Exemplo: gravando um arquivo de saída com a imagem (não é necessário para o trabalho, apenas
-    // para ver o resultado intermediário, por ex, da conversão para tons de cinza)
-    SOIL_save_image("out.bmp", SOIL_SAVE_TYPE_BMP, pic.width/2, pic.height/2,
-        3, (const unsigned char*) pic.img);
-
-
-    // Exemplo: gravando um arquivo saida.html
-    FILE* arq = fopen("saida.html", "w"); // criar o arquivo: w
-    if(arq == NULL) // 0, falso
-    {
-        printf("Erro abrindo arquivo de saída\n");
-        exit(1);
-    }
-
-    fprintf(arq, "<html>\n");
-    fprintf(arq,"<head>\n");
-    fprintf(arq,"</head>\n");
-    fprintf(arq,"<body style=\"background:black;\" leftmargin=0; topmargin=0 >");
-    fprintf(arq,"<style> pre { color: white; font-family:Courier; font-size:12px; letter-spacing:1px}</style>");
-	fprintf(arq,"<pre>");
-	for(int i=0; i<pic.height/4; i++) 
-	{
-		
-		for(int j=0; j<pic.width/2; j++) 
-		{
-			
-			// podemos passar qualquer parametro, pois eles são iguais
-			// multiplico o i pela altura para ter a linha e é somado com a largura 
-			fprintf(arq,"%c", changeToASC(pic.img[(i*pic.width) +j].r));
-		}
-		fprintf(arq,"<br>");
-	}
-	fprintf(arq,"</pre>");
+			fprintf(arq,"</pre>");
 	
-    fprintf(arq,"</body>\n");
-	fprintf(arq, "</html>\n");
+    		fprintf(arq,"</body>\n");
+			fprintf(arq, "</html>\n");
 
-    fclose(arq);
-    free(pic.img);
+    		fclose(arq);
+    		free(pic.img);
+		}    
 }
